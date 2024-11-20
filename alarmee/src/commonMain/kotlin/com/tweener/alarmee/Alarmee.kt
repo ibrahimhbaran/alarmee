72f94f1bd724f183804615eb1c1541cd80c679fe
Vivien Mahé <vivouneuh@gmail.com>
2024-11-20 11:09:14 +0100

    Update Javadoc for Alarmee class

--- alarmee/src/commonMain/kotlin/com/tweener/alarmee/Alarmee.kt
+++ alarmee/src/commonMain/kotlin/com/tweener/alarmee/Alarmee.kt
@@ -12,6 +12,7 @@ import kotlinx.datetime.TimeZone
  * @property scheduledDateTime The specific date and time when the alarm is scheduled to trigger.
  * @property timeZone The time zone in which the alarm should be scheduled. By default, this is set to the system's current time zone.
  * @property repeatInterval The optional interval at which the alarm should repeat (e.g., hourly, daily, weekly). If `null`, the alarm will not repeat.
+ * @property androidNotificationChannelId The notification channel to post the notification on (Only works for Android, this parameter is ignored on iOS).
  *
  * @author Vivien Mahe
  * @since 06/11/2024
