040f2cb300717386d986b9785465771ed4f21af9
Vivien Mahé <vivouneuh@gmail.com>
2024-12-04 18:09:52 +0100

    Update iOS sample

--- sample/iosApp/iosApp/AppDelegate.swift
+++ sample/iosApp/iosApp/AppDelegate.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import GoogleSignIn
 import composeApp
 
 @MainActor
 class AppDelegate : NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         UNUserNotificationCenter.current().delegate = self
@@ -18,5 +23,16 @@ class AppDelegate : NSObject, UIApplicationDelegate, UNUserNotificationCenterDel
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.banner, .badge, .sound])
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        if userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+           let url = userActivity.webpageURL {
+            handleIncomingURL(url)
+            return true
+        }
+        
+        print("No valid URL in user activity.")
+        return false
     }
 }
