a81203cc2cb557ca8524a4ac90e96d01e7b78350
Vivien Mahé <vivouneuh@gmail.com>
2024-12-17 10:37:47 +0100

    Bump lib version to 1.3.5

--- buildSrc/src/main/kotlin/ProjectConfiguration.kt
+++ buildSrc/src/main/kotlin/ProjectConfiguration.kt
@@ -9,7 +9,7 @@ object ProjectConfiguration {
 
     object Alarmee {
         const val packageName = "com.tweener.alarmee"
-        const val versionName = "1.3.4"
+        const val versionName = "1.3.5"
         const val namespace = "$packageName.android"
         const val compileSDK = 34
         const val minSDK = 24
